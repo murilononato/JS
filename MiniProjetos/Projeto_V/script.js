7c34e9783e5dc82fe77482a0cfa28aa8e65f5557
murilononato <mu.nonato@hotmail.com>
2022-12-23 22:21:39 -0300

    sd

--- MiniProjetos/Projeto_V/script.js
+++ MiniProjetos/Projeto_V/script.js
@@ -1,15 +1,58 @@
+var tab = document.getElementById('iLista')
 
-let num = document.getElementById('iNumero')
+var resultado = document.getElementById('iResultados')
 
-let tab = document.getElementById('iLista')
+var num = document.getElementById('num')
 
-let resultado = document.getElementById('iResultados')
+var msg = document.querySelector('div#res')
 
+var valores = []
+
+var teste = Number(num)
 
 function add(){
-    if(isNumero(num.value) && )
+
+
+    // if(isNumero(num.value) && !inLista(num.value , valores)){
+        // valores.push(teste)
+        // let item = document.createElement('option')
+        // item.text = `Valor adicionado ${num.value}`
+        // tab.appendChild(item)
+    // }else{
+    //     window.alert('Valor invalido')
+    // }
+
+    alert(`Teste ${num.value}`)
+
+}
+
+
+function isNumero(n){
+    if(Number(n) >= 1 && Number(n) <= 100){
+
+        alert('1')
+        return true
+    } else {
+        alert('2')
+        return false
+
+    }
+}
+
+function inLista(n, l){
+    if(l.indexOf(Number(n)) != -1){
+        alert('3')
+        return true
+    
+    } else {
+        alert('4')
+        return false
+     
+    }
 }
 
 
 
-
+function Finaliza(){
+  
+}
---
+++ Node/teste.js
@@ -0,0 +1,10 @@
+let num = 120
+
+
+
+function soma(n) {
+   var resultado = num + n
+    console.log(resultado)
+}
+
+soma(2)
